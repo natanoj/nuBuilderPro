79c4dd87912c00de406bc3007cb2c011f416cd6c
gelvo <shane@nusoftware.com>
2016-08-05 14:21:08 +0930

    updated sql files and nusafephp

--- nufileget.php
+++ nufileget.php
@@ -1,14 +1,19 @@
 <?php 
+	$conf = dirname(__FILE__).'/config.php';
+	require($conf);
+	
+	$code 		= $_GET['i'];
+	$sql 		= "SELECT sfi_type, sfi_blob, sfi_name FROM zzzsys_file WHERE sfi_code=:code";
+	$conStr 	= "mysql:host=$nuConfigDBHost;dbname=$nuConfigDBName;charset=utf8";
+	$this_db        = new PDO($conStr, $nuConfigDBUser, $nuConfigDBPassword);
+	$this_db_obj 	= $this_db->prepare($sql);
+	
+	$this_db_obj->execute(array(":code" => $code));
+	$this_db_obj->bindColumn(1, $mime);
+	$this_db_obj->bindColumn(2, $data, PDO::PARAM_LOB);
+	$this_db_obj->bindColumn(3, $name);
+	$this_db_obj->fetch(PDO::FETCH_BOUND);
 
-require_once('nucommon.php');
-
-$s = "SELECT * FROM zzzsys_file WHERE sfi_code = ? ";
-$t = nuRunQuery($s, array($_GET['i']));
-$r = db_fetch_object($t);
-$f = "'Content-type: $r->sfi_type'";
-
-Header($f); 
-
-print $r->sfi_blob; 
-
+header("Content-Type:" . $mime);
+echo $data;
 ?>
