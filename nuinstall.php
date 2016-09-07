6038208b2838730a419cef382ab858628fd0b073
gelvo <shane@nusoftware.com>
2016-09-07 15:19:36 +0930

    updated sql files and nusafephp

--- nuinstall.php
+++ nuinstall.php
@@ -1,7 +1,7 @@
 <?php
 	error_reporting( error_reporting() & ~E_NOTICE );
         require_once("config.php");
-        require_once("nuinstall_lib2.php");
+        require_once("nuinstall_lib.php");
 
         session_start();
 
